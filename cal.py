6440db01c720879bebfbba214e25a27cc55d785a
CIC <791863347@qq.com>
2018-02-09 00:05:03 +0800

    update

--- cal.py
+++ cal.py
@@ -2,8 +2,19 @@ import itertools
 import pandas as pd
 a = [450, 450, 410, 320, 320, 300, 280, 260, 259, 110]
 total = pd.DataFrame(list(itertools.permutations(a,10)))#全排列
-total['first_type'] = total.apply(lambda x: ((x[0:3].sum())>1100)+((x[3:6].sum())>1100)+((x[6:9].sum())>1100)+((x[9:10].sum())>1100), axis=1)#第一种红包分布超过1100的数目：3,3,3,1
-total['second_type'] = total.apply(lambda x: ((x[0:3].sum())>1100)+((x[3:6].sum())>1100)+((x[6:8].sum())>1100)+((x[8:10].sum())>1100), axis=1)#第一种红包分布超过1100的数目：3,3,2,2
+#第一种红包分布方法中各个红包是否大于1100，1个赏金的红包不可能超过
+total.loc[(total[0]+total[1]+total[2])>1100,"type1_1"] = 1
+total.loc[(total[3]+total[4]+total[5])>1100,"type1_2"] = 1
+total.loc[(total[6]+total[7]+total[8])>1100,"type1_3"] = 1
+#第二种红包分布方法中各个红包是否大于1100，1个、2个赏金的红包不可能超过
+total.loc[(total[0]+total[1]+total[2])>1100,"type2_1"] = 1
+total.loc[(total[3]+total[4]+total[5])>1100,"type2_2"] = 1
+total = total.fillna(0)
+#第一种红包分布超过1100的数目：3,3,3,1
+total["first_type"] = total["type1_1"]+total["type1_2"]+total["type1_3"]
+#第一种红包分布超过1100的数目：3,3,2,2
+total['second_type'] =  total["type2_1"]+total["type2_2"]
+
 #有大红包的概率
 P_has_any = (len(total[total["first_type"]>0])+len(total[total["second_type"]>0]) )/len(total)
 #有1个大红包的概率
@@ -17,5 +28,3 @@ print("P_has_any:",P_has_any*100,"%")
 print("P_has_one:",P_has_one*100,"%")
 print("P_has_two:",P_has_two*100,"%")
 print("P_has_three:",P_has_three*100,"%")
-
-total.to_csv("result.csv",index=False)#保存结果
